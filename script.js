3b3356522bd23cc7192fe2a03ba48d4276ff8f9e
GidonDula <110187920+GidonDula@users.noreply.github.com>
2024-05-28 13:32:27 +0300

    fixing sounds

--- script.js
+++ script.js
@@ -481,7 +481,7 @@ window.onload = function () {
     console.log("centrex", centrex);
     console.log("centery", centery);
     console.log("gridpos", gridpos);
-    if (level.tiles[gridpos.x][gridpos.y - 1].type == -1) {
+    if (level.tiles[gridpos.x][gridpos.y - 1].type == -1 && gridpos.y > 0) {
       console.log("floating bubble", player.buble.tileType);
       gridpos = getGridPosition(centrex, centery - level.tileheight);
     }
